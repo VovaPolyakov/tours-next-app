94f14381cb1db218e3ebcd7d50604bee60947ed1
Vladimir <1337vovalenovo@gmail.com>
2022-11-09 03:03:02 +0300

    added adaption for mobile devices

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div className='md:bg-gray-700 md:dark:bg-gray-800 mb-48'>
+    <div className='bg-gray-700 dark:bg-gray-800 mb-48'>
       <div className='w-10/12 m-auto pb-5 pt-5'>
         <div>
-            <div className='flex flex-row items-center justify-between'>
+            <div className='lg:flex-row flex  flex-col items-center justify-between '>
               <Link href='/' className='text-3xl text-slate-100'>LOGO</Link>
               <div>
                 <Link className='pl-5  text-slate-100'  href='/add'>Add-Tours</Link>
