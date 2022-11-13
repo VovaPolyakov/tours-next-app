9daa92ea29d47e68eea8211bcfb6fde1fd4c35ac
Vladimir <1337vovalenovo@gmail.com>
2022-11-14 00:35:07 +0300

    fix search input

--- src/pages/found.tsx
+++ src/pages/found.tsx
@@ -4,22 +4,25 @@ import prisma from '../../prisma/client'
 import Head from 'next/head'
 import { NextPageContext} from 'next'
 
-async function  getServerSideProps(context: NextPageContext){
-  const name = Array.isArray(context.query?.where)
-  ? context.query?.where.pop() : context.query?.where
-  const x = await prisma.tours.findMany({
-      where:{name: name}
-      
+
+export async function getServerSideProps(context:NextPageContext) {
+  if(context.query.where == undefined){
+    return {props:{data:[]}}
+  }
+  const name = Array.isArray(context.query.where) ? context.query.where.pop() : context.query.where
+  let x = await prisma.tours.findMany({
+    where: {
+      name,
+    }
   })
-  const data = x.map(({id,name,time}) => ({
-    name,
-    id,
-    time: time.toLocaleDateString(),
-  }))
+  const data = x.map(({name,id,time}) => 
+    ({name,id,time: time.toLocaleDateString()})
+  )
   return {
     props: {data},
   }
 }
+
 
 
 const Page: NextPage <InferGetServerSidePropsType<typeof getServerSideProps>>  = ({data}) => {
@@ -33,11 +36,10 @@ const Page: NextPage <InferGetServerSidePropsType<typeof getServerSideProps>>  =
 
         <div className='text-white m-auto sm:w-4/5 lg:w-2/5 bg-gray-700 dark:bg-gray-800 p-4 rounded-lg shadow-lg'>
         <ul>
-          {data.length > 0 ? data.map(({name,time,id}) => <li key={id}>Name: {name}, Time: {time.toString()}</li>) : 'Нету такой страны'}
+          {data?.length > 0 ? data.map(({name,time,id}) => <li key={id}>Name: {name}, Time: {time.toString()}</li>) : 'Нету такой страны'}
         </ul>
       </div>
-    </div>
-  )
+    </div>  )
 }
 
 export default Page
