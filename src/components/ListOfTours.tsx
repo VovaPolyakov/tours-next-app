a0ed4b176d9db10a69fa2f05f70060d94bf21e57
Vladimir <1337vovalenovo@gmail.com>
2022-11-21 16:20:35 +0300

    fix style to list

--- src/components/ListOfTours.tsx
+++ src/components/ListOfTours.tsx
@@ -5,7 +5,7 @@ const ListOfTours = (props:any) => {
   return (
     <ul className=" text-left">
       {props.tours.map((tour:any) => {
-        return <div className=" p-4"><li className="text-white font-bold " key={tour.id}>Tour to: {tour.name}, Time: {tour.time}</li></div>;
+        return <div key={tour.id} className=" p-4"><li className="text-white font-bold " key={tour.id}>Tour to: {tour.name}, Time: {tour.time}</li></div>;
       })}
     </ul>
   );
