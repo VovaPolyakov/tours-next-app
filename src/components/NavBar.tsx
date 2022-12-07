15602417801d76710b017ddb858b62520940015b
Vladimir <1337vovalenovo@gmail.com>
2022-12-08 01:08:37 +0300

    fix navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,19 +3,17 @@ import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-gray-700 dark:bg-gray-800 mb-48'>
-      <div className='w-10/12 m-auto pb-5 pt-5'>
-        <div>
-            <div className='flex flex-row items-center justify-between '>
-              <Link href='/' className='text-3xl text-slate-100'>LOGO</Link>
-              <div>
-                <Link className='pl-5  text-slate-100'  href='/add'>Add-Tours</Link>
-                <Link className='pl-5  text-slate-100'  href='/list'>List-Tours</Link>
-              </div>
-            </div>
-        </div>
+  <div className='bg-gray-700 dark:bg-gray-800 mb-48'>
+      <div className='w-10/12 m-auto p-5'>
+          <div className='flex items-center justify-between'>
+                <Link href='/' className=' pr-5 text-3xl text-white'>LOGO</Link>
+                <div>
+                  <Link className='  text-white'  href='/add'>Add-Tours</Link>
+                  <Link className=' text-white'  href='/list'>List-Tours</Link>
+                </div>
+          </div>
       </div>
-    </div>
+  </div>
   )
 }
 
